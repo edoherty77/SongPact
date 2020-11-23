6d8827ddcc66a74899d629615da7197971b46f0d
Evan Doherty <edoherty77@gmail.com>
2020-11-23 05:38:23 -0500

    added bottom navigator icons

--- App.js
+++ App.js
@@ -1,21 +1,110 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import LoggedOut from './app/views/LoggedOut'
+import ButtonIcon from './app/components/ButtonIcon'
+import Dashboard from './app/views/Dashboard'
+import Contacts from './app/views/Contacts'
+import New from './app/views/NewSongPact'
+import Notifications from './app/views/Notifications'
+import Menu from './app/views/Menu'
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <LoggedOut />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Dashboard"
+        tabBarOptions={{
+          activeTintColor: 'gold',
+          // activeBackgroundColor: colors.primary,
+          inactiveTintColor: '#303036',
+          labelStyle: {
+            fontSize: 10,
+            fontWeight: 'bold',
+            paddingBottom: 40,
+          },
+          tabStyle: {
+            height: 100,
+            backgroundColor: '#30BCED',
+          },
+        }}
+      >
+        <Tab.Screen
+          options={{
+            tabBarLabel: 'Home',
+
+            tabBarIcon: ({ color, size }) => (
+              <ButtonIcon
+                name="home-outline"
+                backgroundColor="transparent"
+                iconColor="red"
+              />
+            ),
+          }}
+          name="Dashboard"
+          component={Dashboard}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <ButtonIcon
+                name="account-multiple"
+                backgroundColor="transparent"
+              />
+            ),
+            tabBarLabel: 'Contacts',
+          }}
+          name="Contacts"
+          component={Contacts}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <ButtonIcon
+                name="plus-circle"
+                backgroundColor="transparent"
+                iconColor="#303036"
+              />
+            ),
+            tabBarLabel: '',
+          }}
+          name="New"
+          component={New}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <ButtonIcon name="bell" backgroundColor="transparent" />
+            ),
+            tabBarLabel: 'Notifications',
+          }}
+          name="Notifications"
+          component={Notifications}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <ButtonIcon name="menu" backgroundColor="transparent" />
+            ),
+            tabBarLabel: 'Menu',
+          }}
+          name="Menu"
+          component={Menu}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
+    backgroundColor: '#30BCED',
     // alignItems: 'center',
     // justifyContent: 'center',
   },
--- app/views/Dashboard.js
+++ app/views/Dashboard.js
@@ -1,12 +1,55 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+
+import ButtonIcon from '../components/ButtonIcon'
+import AppButton from '../components/AppButton'
 
 function Dashboard() {
   return (
-    <View>
-      <Text>Dashboard</Text>
-    </View>
+    <SafeAreaView style={styles.screenContainer}>
+      <View style={styles.appHeader}>
+        <Text style={styles.appName}>SongPact</Text>
+
+        <ButtonIcon
+          style={styles.messageBtn}
+          iconColor="#303036"
+          size={45}
+          name="message-text"
+        />
+      </View>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  appHeader: {
+    flexDirection: 'row',
+    // backgroundColor: 'green',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+  appName: {
+    fontSize: 50,
+    // padding: 20,
+    color: '#FFFAFF',
+    fontWeight: 'bold',
+    // backgroundColor: 'blue',
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    backgroundColor: 'red',
+    alignItems: 'flex-end',
+  },
+  messageBtn: {
+    paddingTop: 10,
+    backgroundColor: 'transparent',
+    // backgroundColor: '#FFFAFF',
+  },
+  screenContainer: {
+    flex: 1,
+    backgroundColor: '#30BCED',
+  },
+})
 
 export default Dashboard
