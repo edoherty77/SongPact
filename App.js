6025189cf89bdc1db4b454c6476873e99faff5b5
Evan Doherty <edoherty77@gmail.com>
2020-11-23 02:16:12 -0500

    file structure added

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 
 export default function App() {
   console.log('yo')
   return (
-    <View style={styles.container}>
-      <Text>fuck u</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Dashboard</Text>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'rgb(' + 164 + ',' + 241 + ',' + 164 + ')',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 })
