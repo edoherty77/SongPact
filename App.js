456636bf652a78e2aa16aa93422acf69389db91f
Evan Doherty <edoherty77@gmail.com>
2020-12-05 05:29:33 -0500

    changed file names for consistency

--- App.js
+++ App.js
@@ -7,13 +7,9 @@ import UserContext from './app/context/userContext'
 import AuthNavigator from './app/navigation/AuthNavigator'
 
 import { ApolloProvider } from '@apollo/client'
-import { GET_ALL_USERS } from './app/src/graphql/Queries'
 import { client } from './app/src/graphql/Client'
 
 import DashboardScreen from './app/views/DashboardScreen'
-import SignUnScreen from './app/views/SignUpScreen'
-import RegisterScreen from './app/views/RegisterScreen'
-import LoginScreen from './app/views/LoginScreen'
 
 export default function App({ navigation }) {
   const [user, setUser] = useState(false)
@@ -25,14 +21,14 @@ export default function App({ navigation }) {
   return (
     <>
       <ApolloProvider client={client}>
-        {/* <NavigationContainer> */}
-        {/* <UserContext.Provider value={{ setUser: setUser }}>
+        <NavigationContainer>
+          <UserContext.Provider value={{ setUser: setUser }}>
             {user ? <AppNavigator /> : <AuthNavigator />}
-          </UserContext.Provider> */}
+          </UserContext.Provider>
+        </NavigationContainer>
         {/* <AppNavigator /> */}
-        {/* </NavigationContainer> */}
         {/* <RegisterScreen /> */}
-        <LoginScreen />
+        {/* <LoginScreen /> */}
       </ApolloProvider>
       {/* <StatusBar style={'light'} /> */}
     </>
--- app/views/SignUpScreen.js
+++ app/views/SignUpScreen.js
@@ -1,17 +1,226 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import {
+  StyleSheet,
+  View,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Text,
+} from 'react-native'
+import * as Yup from 'yup'
 
 import Screen from '../components/Screen'
-import AppText from '../components/AppText'
+import Header from '../components/Header'
+import { AppForm, AppFormField, SubmitButton } from '../components/forms'
 
-function SignUp() {
+import { SIGNUP_USER } from '../src/graphql/Queries'
+import { useMutation } from '@apollo/client'
+
+import User from '../stores/user'
+
+import colors from '../config/colors'
+import AppText from '../components/AppText'
+import AppButton from '../components/AppButton'
+import ButtonText from '../components/ButtonText'
+
+const validationSchema = Yup.object().shape({
+  firstName: Yup.string().required().label('First name'),
+  lastName: Yup.string().required().label('Last name'),
+  artistName: Yup.string().required().label('Artist name'),
+  // companyName: Yup.string().label('Artist name'),
+  email: Yup.string().required().email().label('Email'),
+  password: Yup.string().required().label('Password'),
+  password2: Yup.string().oneOf(
+    [Yup.ref('password'), null],
+    'Passwords must match',
+  ),
+})
+
+function SignUpScreen({ navigation }) {
+  const [mutate] = useMutation(SIGNUP_USER)
+
+  // const register = (values) => {
+  // const newUser = new User(values)
+  // console.log(values)
+  // UserStore.addUser(newUser)
+  // }
+  async function submit(values) {
+    const { data } = await mutate({
+      variables: values,
+    })
+    if (data) {
+      console.log(data.signupUser)
+    }
+  }
+
+  const SignUp = () => {}
+
   return (
     <Screen>
-      <AppText fontSize={30} color="black">
-        Hi
-      </AppText>
+      <KeyboardAvoidingView
+        behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+        style={styles.container}
+      >
+        <Header title="Register" noIcon />
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.registerView}>
+            <AppForm
+              initialValues={{
+                firstName: '',
+                lastName: '',
+                artistName: '',
+                companyName: '',
+                email: '',
+                password: '',
+                password2: '',
+              }}
+              onSubmit={(values) => submit(values)}
+              validationSchema={validationSchema}
+            >
+              <View style={styles.fieldView}>
+                <AppFormField
+                  style={styles.input}
+                  name="firstName"
+                  placeholder="First Name*"
+                  autoCorrect={false}
+                  textContentType="givenName"
+                  paddingRight={'8%'}
+                />
+                <AppFormField
+                  style={styles.input}
+                  name="lastName"
+                  placeholder="Last Name*"
+                  autoCorrect={false}
+                  textContentType="familyName"
+                  paddingRight={'17%'}
+                />
+                <AppFormField
+                  style={styles.input}
+                  name="artistName"
+                  placeholder="Artist Name*"
+                  autoCorrect={false}
+                  // paddingRight={"5%"}
+                />
+                <AppFormField
+                  style={styles.input}
+                  name="companyName"
+                  placeholder="Company Name"
+                  autoCorrect={false}
+                  // paddingRight={"5%"}
+                />
+                <AppFormField
+                  style={styles.input}
+                  name="email"
+                  placeholder="Email*"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  textContentType="emailAddress"
+                  keyboardType="email-address"
+                  // paddingRight={"5%"}
+                />
+                <AppFormField
+                  style={styles.input}
+                  name="password"
+                  placeholder="Password*"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  textContentType="password"
+                  secureTextEntry
+                />
+                <AppFormField
+                  style={styles.input}
+                  name="password2"
+                  placeholder="Confirm Password*"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  textContentType="password"
+                  secureTextEntry
+                />
+              </View>
+              <View style={styles.submitView}>
+                <SubmitButton
+                  style={styles.createButton}
+                  title="Create Profile"
+                  color={colors.confirm}
+                  dismissKey={Keyboard.dismiss}
+                />
+              </View>
+              <View style={styles.loginView}>
+                <Text style={{ fontSize: 20, fontFamily: 'Futura' }}>
+                  Already have an accout with us?{' '}
+                </Text>
+                <ButtonText
+                  title="Login"
+                  color={colors.red}
+                  fontSize={20}
+                  onPress={() => navigation.navigate('SignIn')}
+                />
+              </View>
+            </AppForm>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     </Screen>
   )
 }
 
-export default SignUp
+export default SignUpScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: colors.lttan,
+  },
+  registerView: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    // backgroundColor: 'gray',
+    height: '100%',
+    flex: 1,
+  },
+  fieldView: {
+    // backgroundColor: 'gray',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 5,
+  },
+  input: {
+    width: '95%',
+    backgroundColor: colors.gray,
+    fontSize: 18,
+    paddingLeft: 20,
+    height: 45,
+    borderRadius: 25,
+  },
+  submitView: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+
+    width: '95%',
+    // backgroundColor: 'green',
+    flex: 1,
+  },
+  createButton: {
+    marginTop: 20,
+    borderRadius: 50,
+    height: 50,
+    backgroundColor: colors.red,
+  },
+  loginView: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    // backgroundColor: 'blue',
+    width: '90%',
+    marginTop: 10,
+    flex: 1,
+  },
+})
