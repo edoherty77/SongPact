670d9875d682d53d17e5632d41f7341af7ac55ee
Evan Doherty <edoherty77@gmail.com>
2020-12-10 14:10:27 -0500

    signup, confirm sign up, and sign in working

--- App.js
+++ App.js
@@ -1,63 +1,68 @@
-import { StatusBar } from "expo-status-bar"
-import React, { useEffect, useState } from "react"
+import { StatusBar } from 'expo-status-bar'
+import React, { useEffect, useState } from 'react'
 
 // AMPLIFY & AUTH
-import Amplify, { Auth } from "aws-amplify"
-import awsconfig from "./aws-exports"
+import Amplify, { Auth } from 'aws-amplify'
+import awsconfig from './aws-exports'
 Amplify.configure({
   ...awsconfig,
   Analytics: {
     disabled: true, // kills unhandled promise warning
   },
 })
-import { withAuthenticator } from "aws-amplify-react-native"
+import { withAuthenticator } from 'aws-amplify-react-native'
 
 // NAV
-import { NavigationContainer } from "@react-navigation/native"
-import AppNavigator from "./app/navigation/AppNavigator"
+import { NavigationContainer } from '@react-navigation/native'
+import AppNavigator from './app/navigation/AppNavigator'
+import AuthNavigator from './app/navigation/AuthNavigator'
 
 // DATA FLOW
-import AsyncStorage from "@react-native-async-storage/async-storage"
-import { ApolloProvider } from "@apollo/client"
-import { client } from "./app/src/graphql/Client"
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { ApolloProvider } from '@apollo/client'
+import { client } from './app/src/graphql/Client'
 
 function App({ navigation }) {
   const [user, setUser] = useState(null)
 
-  const getCurrentUser = async () => {
-    try {
-      const user = await Auth.currentAuthenticatedUser()
-      setUser(user.attributes) // TODO remove or move to user store
-      // look for user ID that matches sub ID
-      // if found
-      // return foundUser data
-      // store foundUser data in state store
-      // if not found
-      // create newUser entry with ID == sub
-      // store newUser data in state
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  // const getCurrentUser = async () => {
+  //   try {
+  // const user = await Auth.currentAuthenticatedUser()
+  // setUser(user.attributes)
+  // TODO remove or move to user store
+  // look for user ID that matches sub ID
+  // if found
+  // return foundUser data
+  // store foundUser data in state store
+  // if not found
+  // create newUser entry with ID == sub
+  // store newUser data in state
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
 
-  if (user) {
-    console.log("user///", user)
-  }
+  // if (user) {
+  //   console.log('user///', user)
+  // }
 
-  useEffect(() => {
-    getCurrentUser()
-  }, [])
+  // useEffect(() => {
+  //   getCurrentUser()
+  // }, [])
 
   return (
     <>
       <ApolloProvider client={client}>
         <NavigationContainer>
-          <AppNavigator />
+          {/* <AppNavigator /> */}
+
+          <AuthNavigator />
         </NavigationContainer>
       </ApolloProvider>
-      <StatusBar style={"auto"} />
+      <StatusBar style={'auto'} />
     </>
   )
 }
 
-export default withAuthenticator(App)
+export default App
+// export default withAuthenticator(App)
--- app/views/SignUpScreen.js
+++ app/views/SignUpScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState } from 'react'
 import {
   StyleSheet,
   View,
@@ -7,66 +7,75 @@ import {
   Keyboard,
   Text,
   ImageBackground,
-} from "react-native"
-import * as Yup from "yup"
+} from 'react-native'
+import * as Yup from 'yup'
+import NativeModal from 'react-native-modal'
 
-import Screen from "../components/Screen"
-import Header from "../components/Header"
-import { AppForm, AppFormField, SubmitButton } from "../components/forms"
+import Screen from '../components/Screen'
+import Header from '../components/Header'
 
-import { SIGNUP_USER } from "../src/graphql/Queries"
-import { useMutation } from "@apollo/client"
+import { AppForm, AppFormField, SubmitButton } from '../components/forms'
 
-import colors from "../config/colors"
-import AppText from "../components/AppText"
-import AppButton from "../components/AppButton"
-import ButtonText from "../components/ButtonText"
+import { Auth } from 'aws-amplify'
+
+import { SIGNUP_USER } from '../src/graphql/Queries'
+import { useMutation } from '@apollo/client'
+
+import colors from '../config/colors'
+import AppText from '../components/AppText'
+import AppButton from '../components/AppButton'
+import ButtonText from '../components/ButtonText'
 
 const validationSchema = Yup.object().shape({
-  firstName: Yup.string().required().label("First name"),
-  lastName: Yup.string().required().label("Last name"),
-  artistName: Yup.string().required().label("Artist name"),
+  firstName: Yup.string().required().label('First name'),
+  lastName: Yup.string().required().label('Last name'),
+  artistName: Yup.string().required().label('Artist name'),
   // companyName: Yup.string().label('Artist name'),
-  email: Yup.string().required().email().label("Email"),
-  password: Yup.string().required().label("Password"),
+  email: Yup.string().required().email().label('Email'),
+  password: Yup.string().required().label('Password'),
   password2: Yup.string().oneOf(
-    [Yup.ref("password"), null],
-    "Passwords must match"
+    [Yup.ref('password'), null],
+    'Passwords must match',
   ),
 })
 
 function SignUpScreen({ navigation }) {
-  const [mutate] = useMutation(SIGNUP_USER)
-
-  // const register = (values) => {
-  // const newUser = new User(values)
-  // console.log(values)
-  // UserStore.addUser(newUser)
-  // }
+  const [modalVisible, setModalVisible] = useState(false)
   async function submit(values) {
-    const { data } = await mutate({
-      variables: values,
-    })
-    if (data) {
-      console.log(data.signupUser)
+    try {
+      const { user } = await Auth.signUp({
+        username: values.email,
+        password: values.password,
+        attributes: {
+          email: values.email,
+        },
+      })
+      if (user) {
+        // const { data } = await mutate({
+        //   variables: values,
+        // })
+        setModalVisible(true)
+      }
+    } catch (error) {
+      console.log(error)
     }
   }
 
-  const SignUp = () => {}
+  async function verify(values) {
+    const authCode = values.code
+    const username = values.email
+    // console.log(authCode)
+    Auth.confirmSignUp(username, authCode)
+      .then((data) => console.log(data))
+      .then(() => setModalVisible(false))
+      .then(() => navigation.navigate('SignIn'))
+      .catch((err) => console.log(err))
+  }
 
   return (
     <Screen>
-      {/* <ImageBackground
-        imageStyle={{ opacity: 0.4 }}
-        style={{
-          flex: 1,
-          resizeMode: 'cover',
-          justifyContent: 'center',
-        }}
-        source={require('../assets/pic1.jpeg')}
-      > */}
       <KeyboardAvoidingView
-        behavior={Platform.OS == "ios" ? "padding" : "height"}
+        behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
         style={styles.container}
       >
         <Header title="Register" noIcon />
@@ -74,13 +83,13 @@ function SignUpScreen({ navigation }) {
           <View style={styles.registerView}>
             <AppForm
               initialValues={{
-                firstName: "",
-                lastName: "",
-                artistName: "",
-                companyName: "",
-                email: "",
-                password: "",
-                password2: "",
+                firstName: '',
+                lastName: '',
+                artistName: '',
+                companyName: '',
+                email: '',
+                password: '',
+                password2: '',
               }}
               onSubmit={(values) => submit(values)}
               validationSchema={validationSchema}
@@ -92,7 +101,7 @@ function SignUpScreen({ navigation }) {
                   placeholder="First Name*"
                   autoCorrect={false}
                   textContentType="givenName"
-                  paddingRight={"8%"}
+                  paddingRight={'8%'}
                 />
                 <AppFormField
                   style={styles.input}
@@ -100,7 +109,7 @@ function SignUpScreen({ navigation }) {
                   placeholder="Last Name*"
                   autoCorrect={false}
                   textContentType="familyName"
-                  paddingRight={"17%"}
+                  paddingRight={'17%'}
                 />
                 <AppFormField
                   style={styles.input}
@@ -154,21 +163,71 @@ function SignUpScreen({ navigation }) {
                 />
               </View>
               <View style={styles.loginView}>
-                <Text style={{ fontSize: 20, fontFamily: "Futura" }}>
-                  Already have an accout with us?{" "}
+                <Text style={{ fontSize: 20, fontFamily: 'Futura' }}>
+                  Already have an accout with us?{' '}
                 </Text>
                 <ButtonText
                   title="Login"
                   color={colors.red}
                   fontSize={20}
-                  onPress={() => navigation.navigate("SignIn")}
+                  onPress={() => navigation.navigate('SignIn')}
                 />
               </View>
             </AppForm>
+            <NativeModal
+              hasBackdrop={true}
+              isVisible={modalVisible}
+              animationType="slide"
+              // onBackdropPress={() => setModalVisible(false)}
+              // backdropColor="blue"
+              backdropOpacity={0.6}
+              // onModalHide={() => {
+              //   getLocation(), refreshMap(100)
+              // }}
+              style={styles.confirmModal}
+            >
+              <View style={styles.modalView}>
+                <AppText fontWeight="bold" color={colors.black} fontSize={25}>
+                  Verify Your Account
+                </AppText>
+                <AppForm
+                  initialValues={{
+                    email: '',
+                    code: '',
+                  }}
+                  onSubmit={(values) => verify(values)}
+                  // validationSchema={validationSchema}
+                >
+                  <View style={styles.fieldView}>
+                    <AppFormField
+                      style={[styles.input, { width: '100%' }]}
+                      name="email"
+                      autoCapitalize="none"
+                      placeholder="Email"
+                      autoCorrect={false}
+                      textContentType="emailAddress"
+                      keyboardType="email-address"
+                      paddingRight={'8%'}
+                    />
+                    <AppFormField
+                      style={[styles.input, { width: '100%' }]}
+                      name="code"
+                      placeholder="Verification Code"
+                      paddingRight={'8%'}
+                    />
+                    <SubmitButton
+                      style={[{ width: '100%' }, styles.createButton]}
+                      title="Submit"
+                      color={colors.confirm}
+                      dismissKey={Keyboard.dismiss}
+                    />
+                  </View>
+                </AppForm>
+              </View>
+            </NativeModal>
           </View>
         </TouchableWithoutFeedback>
       </KeyboardAvoidingView>
-      {/* </ImageBackground> */}
     </Screen>
   )
 }
@@ -178,28 +237,28 @@ export default SignUpScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     backgroundColor: colors.lttan,
   },
   registerView: {
-    width: "100%",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-    height: "100%",
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '100%',
     flex: 1,
   },
   fieldView: {
-    width: "100%",
-    justifyContent: "center",
-    alignItems: "center",
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
     flex: 5,
   },
   input: {
-    width: "95%",
+    width: '95%',
     backgroundColor: colors.gray,
     fontSize: 18,
     paddingLeft: 20,
@@ -207,11 +266,10 @@ const styles = StyleSheet.create({
     borderRadius: 25,
   },
   submitView: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-
-    width: "95%",
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    width: '95%',
     flex: 1,
   },
   createButton: {
@@ -221,11 +279,26 @@ const styles = StyleSheet.create({
     backgroundColor: colors.red,
   },
   loginView: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    width: "90%",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '90%',
     marginTop: 10,
     flex: 1,
+  },
+  confirmModal: {
+    backgroundColor: colors.lttan,
+    marginHorizontal: 50,
+    marginTop: 200,
+    marginBottom: 250,
+    padding: 0,
+    borderRadius: 30,
+  },
+  modalView: {
+    height: 350,
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20,
   },
 })
