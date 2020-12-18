ad6e24e709427b27c30703bbe51124da3e2475c1
Evan Doherty <edoherty77@gmail.com>
2020-12-18 05:18:15 -0500

    set up adding collaborators

--- app/components/ContactCheckBox.js
+++ app/components/ContactCheckBox.js
@@ -1,29 +1,13 @@
 import React, { useState } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { useFormikContext } from 'formik'
-import AppText from '../components/AppText'
-import Separator from '../components/Separator'
-import colors from '../config/colors'
-import ButtonIcon from '../components/ButtonIcon'
 import RNCheckboxCard from 'react-native-checkbox-card'
-import { strike } from 'aws-amplify'
-import { check } from 'graphql-anywhere'
 
 const ContactCheckBox = ({ children, title, onPress, name }) => {
-  const {
-    setFieldValue,
-    handleChange,
-    errors,
-    touched,
-    values,
-  } = useFormikContext()
   const [isSelected, setSelection] = useState(false)
 
   return (
     <View style={styles.mainView}>
-      {/* <AppText onPress={onPress} fontSize={25}>
-        {title}
-      </AppText> */}
       <RNCheckboxCard
         text={title}
         backgroundColor="pink"
--- app/views/CreatePact/AddCollaborator.js
+++ app/views/CreatePact/AddCollaborator.js
@@ -6,12 +6,14 @@ import {
   TextInput,
   Text,
   FlatList,
+  SafeAreaView,
 } from 'react-native'
 import { Header, Item, Icon, Input } from 'native-base'
 import Head from '../../components/Header'
 import Screen from '../../components/Screen'
 import AppText from '../../components/AppText'
 import ContactCheckBox from '../../components/ContactCheckBox'
+import UserIcon from '../../components/UserIcon'
 
 import { Formik, Field, Form, FieldArray } from 'formik'
 import AppButton from '../../components/AppButton'
@@ -24,83 +26,113 @@ const contacts = [
   { first: 'Matt', last: 'O', id: 5 },
   { first: 'Tom', last: 'Johnson', id: 6 },
   { first: 'Kyle', last: 'Mooney', id: 7 },
+  { first: 'Kyle', last: 'Mooney', id: 8 },
+  { first: 'Kyle', last: 'Mooney', id: 9 },
 ]
 
-function AddCollaborator() {
+function AddCollaborator({ navigation }) {
   const [people, setPeople] = useState([])
   // const [isChecked, setIsChecked] = useState(false)
 
   function chooseCollab(person) {
+    console.log(person)
     setPeople([
       ...people,
       {
+        name: person.first,
         id: people.length,
-        person,
       },
     ])
-    console.log({ people })
   }
+
+  function submit(values) {
+    // console.log(values)
+  }
+
   return (
     <Screen>
-      <Head title="gay" />
+      <Head
+        title="Pick Peeps"
+        name="arrow-right-bold"
+        iconPress={() => navigation.navigate('Second')}
+        icon="arrow-left-bold"
+        back={() => navigation.navigate('First')}
+      />
 
-      <View>
+      <View style={styles.mainView}>
         <Formik
           initialValues={{ collabs: [] }}
-          onSubmit={(values) => alert(JSON.stringify(values))}
+          onSubmit={(values) => submit(values)}
         >
           {({ values, handleSubmit }) => (
-            <View>
-              <FieldArray name="collabs">
-                {({ push, remove }) => (
-                  <FlatList
-                    data={values.collabs}
-                    keyExtractor={(collab) => collab.id}
-                    renderItem={({ item, index }) => (
-                      <ContactCheckBox
-                        name={`collabs.${index}`}
-                        title={`${item.first} ${item.last}`}
-                        onPress={() => {
-                          push(item), chooseCollab(item)
-                        }}
-                      />
-                    )}
-                  />
-                )}
-              </FieldArray>
-              <Header
-                transparent={true}
-                searchBar
-                noshadow
-                rounded
-                width={300}
-                alignSelf="center"
-              >
-                <Item>
-                  <Icon name="ios-search" />
-                  <Input placeholder="Search" />
-                  <Icon name="ios-people" />
-                </Item>
-              </Header>
-              <FieldArray name="collabs">
-                {({ push, remove }) => (
-                  <FlatList
-                    data={contacts}
-                    keyExtractor={(contact) => contact.id}
-                    renderItem={({ item, index }) => (
-                      <ContactCheckBox
-                        name={`collabs.${index}`}
-                        title={`${item.first} ${item.last}`}
-                        onPress={() => {
-                          push(item), chooseCollab(item)
-                        }}
-                      />
-                    )}
-                  />
-                )}
-              </FieldArray>
-
-              <Button title="submit" onPress={handleSubmit} />
+            <View style={styles.formView}>
+              <View style={styles.inputView}>
+                <Header
+                  transparent={true}
+                  searchBar
+                  noshadow
+                  rounded
+                  width={300}
+                  alignSelf="center"
+                >
+                  <Item>
+                    <Icon name="ios-search" />
+                    <Input placeholder="Search" />
+                    <Icon name="ios-people" />
+                  </Item>
+                </Header>
+              </View>
+              <View style={styles.addedCollabView}>
+                <FieldArray name="collabs">
+                  {({ push, remove }) => (
+                    <FlatList
+                      horizontal={true}
+                      contentContainerStyle={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        // backgroundColor: 'blue',
+                        width: '100%',
+                      }}
+                      style={styles.addedCollabsList}
+                      data={values.collabs}
+                      keyExtractor={(collab) => collab.id}
+                      renderItem={({ item, index }) => (
+                        <UserIcon
+                          name={`collabs.${index}`}
+                          title={`${item.first} ${item.last}`}
+                          onPress={() => {
+                            push(item), chooseCollab(item)
+                          }}
+                        />
+                      )}
+                    />
+                  )}
+                </FieldArray>
+              </View>
+              <View style={styles.contactsView}>
+                <FieldArray name="collabs">
+                  {({ push, remove }) => (
+                    <FlatList
+                      style={styles.contactsList}
+                      contentContainerStyle={{ flexGrow: 1 }}
+                      data={contacts}
+                      keyExtractor={(contact) => contact.id}
+                      renderItem={({ item, index }) => (
+                        <ContactCheckBox
+                          name={`collabs.${index}`}
+                          title={`${item.first} ${item.last}`}
+                          onPress={() => {
+                            push(item), chooseCollab({ item })
+                          }}
+                        />
+                      )}
+                    />
+                  )}
+                </FieldArray>
+              </View>
+              <View style={styles.submitView}>
+                <Button title="submit" onPress={handleSubmit} />
+              </View>
             </View>
           )}
         </Formik>
@@ -110,13 +142,41 @@ function AddCollaborator() {
 }
 
 const styles = StyleSheet.create({
+  mainView: {
+    // backgroundColor: 'yellow',
+    display: 'flex',
+    flex: 1,
+  },
+  formView: {
+    // backgroundColor: 'gray',
+    flex: 1,
+  },
+  addedCollabView: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: 70,
+    // flex: 1,
+  },
+  addedCollabsList: {
+    // flexWrap: 'wrap',
+  },
   inputView: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     width: '100%',
-    marginBottom: 5,
+    // marginBottom: 5,
+    height: 50,
     marginTop: 5,
+    // backgroundColor: 'purple',
+  },
+  contactsView: {
+    // backgroundColor: 'orange',
+  },
+  submitView: {
+    position: 'absolute',
+    top: 60,
+    right: 20,
   },
 })
 
