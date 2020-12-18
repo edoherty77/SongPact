dbed1748cc97a1de3bf7ab434e1c1e4ec2216a37
sjaugmented <sjaugmented@gmail.com>
2020-12-18 14:44:15 -0800

    custom signUp is working

--- app/views/Auth/AppSignUp.js
+++ app/views/Auth/AppSignUp.js
@@ -4,11 +4,32 @@ import { Auth } from "aws-amplify"
 import { SafeAreaView } from "react-native-safe-area-context"
 import AppTextInput from "../../components/AppTextInput"
 import AppButton from "../../components/AppButton"
+import Screen from "../../components/Screen"
+import { KeyboardAvoidingView } from "react-native"
+import Header from "../../components/Header"
+import { TouchableWithoutFeedback } from "react-native"
+import { Keyboard } from "react-native"
+import { Platform } from "react-native"
+import * as Yup from "yup"
+
+const validationSchema = Yup.object().shape({
+  firstName: Yup.string().required().label("First name"),
+  lastName: Yup.string().required().label("Last name"),
+  artistName: Yup.string().required().label("Artist name"),
+  // companyName: Yup.string().label('Artist name'),
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().label("Password"),
+  password2: Yup.string().oneOf(
+    [Yup.ref("password"), null],
+    "Passwords must match"
+  ),
+})
 
 export default function SignUp({ navigation }) {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   //   const [email, setEmail] = useState("")
+
   async function signUp() {
     try {
       await Auth.signUp({ username, password, attributes: { email: username } })
@@ -18,30 +39,37 @@ export default function SignUp({ navigation }) {
       console.log("❌ Error signing up...", error)
     }
   }
+
   return (
-    <SafeAreaView style={styles.safeAreaContainer}>
-      <View style={styles.container}>
-        <Text style={styles.title}>Create a new account</Text>
-        <AppTextInput
-          value={username}
-          onChangeText={(text) => setUsername(text)}
-          icon="email"
-          placeholder="Email"
-          autoCapitalize="none"
-          keyboardType="email-address"
-          textContentType="emailAddress"
-        />
-        <AppTextInput
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-          icon="lock"
-          placeholder="Password"
-          autoCapitalize="none"
-          autoCorrect={false}
-          secureTextEntry
-          textContentType="password"
-        />
-        {/* <AppTextInput
+    <Screen>
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+        style={styles.container}
+      >
+        <Header title="Register" noIcon />
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.registerView}>
+            <AppTextInput
+              value={username}
+              onChangeText={(text) => setUsername(text)}
+              icon="email"
+              placeholder="Email"
+              autoCapitalize="none"
+              autoCorrect={false}
+              keyboardType="email-address"
+              textContentType="emailAddress"
+            />
+            <AppTextInput
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              icon="lock"
+              placeholder="Password"
+              autoCapitalize="none"
+              autoCorrect={false}
+              secureTextEntry
+              // textContentType="password" // TODO uncomment!
+            />
+            {/* <AppTextInput
           value={email}
           onChangeText={(text) => setEmail(text)}
           icon="email"
@@ -50,16 +78,18 @@ export default function SignUp({ navigation }) {
           keyboardType="email-address"
           textContentType="emailAddress"
         /> */}
-        <AppButton title="Sign Up" onPress={signUp} />
-        <View style={styles.footerButtonContainer}>
-          <TouchableOpacity onPress={() => navigation.navigate("SignIn")}>
-            <Text style={styles.forgotPasswordButtonText}>
-              Already have an account? Sign In
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </SafeAreaView>
+            <AppButton title="Sign Up" onPress={signUp} />
+            <View style={styles.footerButtonContainer}>
+              <TouchableOpacity onPress={() => navigation.navigate("SignIn")}>
+                <Text style={styles.forgotPasswordButtonText}>
+                  Already have an account? Sign In
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </Screen>
   )
 }
 
