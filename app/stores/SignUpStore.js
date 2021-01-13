97d2fa7b973aa6dd1db8a80ed485682bf467b05b
sjaugmented <sjaugmented@gmail.com>
2021-01-13 07:40:00 -0800

    +SignUpStore for expanded registration

---
+++ app/stores/SignUpStore.js
@@ -0,0 +1,52 @@
+import { makeAutoObservable } from "mobx"
+
+class SignUpStore {
+  firstName = ""
+  lastName = ""
+  email = ""
+  password = ""
+  address = ""
+  city = ""
+  state = ""
+  zipCode = ""
+  artistName = ""
+  companyName = ""
+
+  setScreen1(values) {
+    console.log("storing first SignUp screen")
+    console.log(values)
+    this.firstName = values.firstName
+    this.lastName = values.lastName
+    this.email = values.email
+    this.password = values.password
+  }
+
+  setScreen2(values) {
+    console.log("storing second SignUp screen")
+    console.log(values)
+    this.address = values.address
+    this.city = values.city
+    this.state = values.state
+    this.zipCode = values.zipCode
+  }
+
+  setScreen3(values) {
+    console.log("storing last SignUp screen")
+    console.log(values)
+    this.artistName = values.artistName
+    this.companyName = values.companyName
+  }
+
+  resetSignUp() {
+    this.firstName = ""
+    this.lastName = ""
+    this.email = ""
+    this.password = ""
+    this.address = ""
+    this.city = ""
+    this.state = ""
+    this.zipCode = ""
+    this.artistName = ""
+    this.companyName = ""
+  }
+}
