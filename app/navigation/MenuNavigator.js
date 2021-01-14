8d94fb03a4398e1c70ff14311db6adb1a7168f59
sjaugmented <sjaugmented@gmail.com>
2021-01-13 17:20:38 -0800

    +MenuNavigator

---
+++ app/navigation/MenuNavigator.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { createStackNavigator } from "@react-navigation/stack"
+
+const MenuNavigator = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default MenuNavigator
